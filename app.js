1c8f51480a5aa7aa5d4fa7d69bc723bc53285264
David Bernad <david.bernad@gbeasts.com>
2023-07-11 10:09:38 +0200

    Step 1 again

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/users', usersRouter);
 
 console.log("Funcionando");
 
-/*********************************************/
+/*********************************************
 //Añadimos desarrollo de Agravain
 //const express = require('express');
 const { ActivityTypes } = require('botbuilder');
@@ -41,7 +41,7 @@ const adapter = new BotFrameworkAdapter({
 app.listen(process.env.PORT || 3978, () => {
   console.log(`Servidor escuchando en el puerto ${process.env.PORT || 3978}`);
 });
-/*
+
 const url = 'https://dev-agravainbot.cognitiveservices.azure.com/language/:analyze-conversations?projectName=Agravain-Orchestation&deploymentName=Agravain-Orchestation-Deployment-v0.1&api-version=2022-10-01-preview';
 const subscriptionKey = '00e95f52cb4e409dad41116c1e15593a';
 const requestId = '4ffcac1c-b2fc-48ba-bd6d-b69d9942995a';
